440b41a3316cb16f29a32cca40c2af047e803bdd
Shadowdragon019 <67480398+Shadowdragon019@users.noreply.github.com>
2022-11-04 23:56:47 -0500

    Initial commit uwu

---
+++ src/main/java/SimplyPaxels.java
@@ -0,0 +1,14 @@
+import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+public class SimplyPaxels implements ModInitializer {
+    public static final String id = "simply_paxles";
+    public static final Logger logger = LogManager.getLogger(id);
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
