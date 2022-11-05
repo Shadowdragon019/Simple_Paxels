cfe37adfc949494e07aa7481453e67f1eb92978d
Shadowdragon019 <67480398+Shadowdragon019@users.noreply.github.com>
2022-11-04 23:58:21 -0500

    Fixed modid

--- src/main/java/SimplyPaxels.java
+++ src/main/java/SimplyPaxels.java
@@ -4,7 +4,7 @@ import org.apache.logging.log4j.Logger;
 
 
 public class SimplyPaxels implements ModInitializer {
-    public static final String id = "simply_paxles";
+    public static final String id = "simply_paxels";
     public static final Logger logger = LogManager.getLogger(id);
 
     @Override
